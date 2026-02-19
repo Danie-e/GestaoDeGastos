0e96fc6e2d211d9f14d5b7b19eb4feef9c7c1c8a
Danie-e <danielaeloisa1@gmail.com>
2026-02-18 21:08:11 -0300

    Criando modelo para pessoa e rota GET e POST

---
+++ ApiGastos/Controllers/PessoaController.cs
@@ -0,0 +1,37 @@
+﻿using ApiGastos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGastos.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PessoaController : Controller
+{
+    private static List<Pessoa> Pessoas = new List<Pessoa>()
+    {
+         new(){
+            DataNescimento = "28/09/2001",
+            Nome = "Daniela",
+            Identificador = 1,
+         },
+    };
+
+    [HttpPost]
+    public void AdicionarPessoa([FromBody] Pessoa pessoa)
+    {
+
+        Pessoas.Add(pessoa);
+    }
+
+    [HttpGet]
+    public IEnumerable<Pessoa> ListarPessoas()
+    {
+        return Pessoas;
+    }
+
+    [HttpGet("{id}")]
+    public Pessoa RetornaPessoaPorId(int id)
+    {
+        return Pessoas.FirstOrDefault(p => p.Identificador.Equals(id))!;
+    }
+}
---
+++ ApiGastos/Models/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiGastos.Models;
+
+public class Pessoa
+{
+    public int Identificador { get; set; }
+
+    [Required(ErrorMessage = "O nome da pessoa é obrigatorio.")]
+    [MaxLength(200, ErrorMessage = "O nome da pessoa não pode exceder 200 caracteres.")]
+    public string Nome { get; set; }
+
+    [Required]
+    public string DataNescimento { get; set; }
+    public int Idade
+    {
+        get
+        {
+            string[] valores = DataNescimento.Split("/");
+            int dia = int.Parse(valores[0]);
+            int mes = int.Parse(valores[1]);
+            int ano = int.Parse(valores[2]);
+
+            int idade = DateTime.Now.Year - new DateOnly(ano, mes, dia).Year;
+
+            if ((DateTime.Now.Day < dia) && DateTime.Now.Month < mes)
+                idade--;
+
+            return idade;
+        }
+    }
+}
